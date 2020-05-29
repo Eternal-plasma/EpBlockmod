1c8f68d83f9884dbd4356f3394663b15cb828ac0
Eternal_plasma <eternal.plasma1634@gmail.com>
2020-05-28 23:34:50 -0500

    add

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-minetest.register_node("mymod:name", {
-  description = "Myblock",
-  tiles = {"l.png"},
+minetest.register_node("epf:block1", {
+  description = "Pattern1",
+  tiles = {"1.png"},
   groups = {cracky = 3, wood = 1},
 })
-minetest.register_alias("Myblock", "mymod:name")
+minetest.register_alias("Pattern1", "epf:block1")
