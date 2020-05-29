ecb9e4d2a5e91749658a79794bff4deed761b62a
Eternal_plasma <eternal.plasma1634@gmail.com>
2020-05-29 14:50:56 -0500

    add

--- init.lua
+++ init.lua
@@ -4,3 +4,27 @@ minetest.register_node("epf:block1", {
   groups = {cracky = 3, wood = 1},
 })
 minetest.register_alias("Pattern1", "epf:block1")
+minetest.register_node("epf:block2", {
+  description = "Pattern2",
+  tiles = {"2.png"},
+  groups = {cracky = 3, wood = 1},
+})
+minetest.register_alias("Pattern2", "epf:block2")
+minetest.register_node("epf:block3", {
+  description = "Pattern3",
+  tiles = {"3.png"},
+  groups = {cracky = 3, wood = 1},
+})
+minetest.register_alias("Pattern3", "epf:block3")
+minetest.register_node("epf:block4", {
+  description = "Pattern4",
+  tiles = {"4.png"},
+  groups = {cracky = 3, wood = 1},
+})
+minetest.register_alias("Pattern4", "epf:block4")
+minetest.register_node("epf:block5", {
+  description = "Pattern5",
+  tiles = {"5.png"},
+  groups = {cracky = 3, wood = 1},
+})
+minetest.register_alias("Pattern5", "epf:block5")
